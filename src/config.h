46f12800010486137cf55e47337bdd0d9042899a
BuildTools <unconfigured@null.spigotmc.org>
2022-11-15 00:42:33 +0200

    Added debug options

---
+++ src/config.h
@@ -0,0 +1,29 @@
+/* Definitions */
+
+//#define DEBUG                    // Uncomment to enable debug messages
+
+#define MAX_STRIPS 2                // Max led strips
+
+#define NUM_OF_MODES 1              // Number of modes
+
+#define NUM_OF_PALETTES 1           // Number of color palettes
+
+/*-------------*/
+
+
+/* Debug settings */
+
+/* 1 to enable */
+#define CONF_ENABLE_DEBUG   1
+#define CONF_ENABLE_INFO    1
+#define CONF_ENABLE_WARN    1
+#define CONF_ENABLE_ERROR   1
+
+/* Enables color for terminal */
+#define CONF_ENABE_TERMINAL_COLOR 1
+
+/* Select text color for debug output*/
+#define CONF_DEBUG_COLOR WHITE 
+#define CONF_INFO_COLOR CYAN
+#define CONF_WARN_COLOR YELLOW
+#define CONF_ERROR_COLOR RED 
