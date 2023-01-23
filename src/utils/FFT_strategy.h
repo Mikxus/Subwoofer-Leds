07f6cf08eb3f83adec04e07b2131c0c03aff5cd2
Mikxus <91991837+Mikxus@users.noreply.github.com>
2023-01-24 01:17:58 +0200

    FFT template for different backends

---
+++ src/utils/FFT_strategy.h
@@ -0,0 +1,87 @@
+/*
+ * MIT License
+ * 
+ * Copyright (c) 2023 Mikko Johannes Heinänen 
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+*/
+#ifndef _FFT_STRATEGY_H_
+#define _FFT_STRATEGY_H_
+
+#include "../lib/rISR/src/rISR.h"
+
+/**
+ * @brief Enum for implemented backend
+ * 
+ */
+typedef enum {
+    fixed_8,
+} fft_backend;
+
+/**
+ * @brief FFT abstraction layer
+ * 
+ */
+class FFT_backend_template
+{
+protected:
+    uint16_t m_sample_size = 0;
+    void *m_data;
+
+    /**
+     * @brief Allocates array of custom data size * sample size. Then sets data to point to it
+     * 
+     * @return true Allocation succesfull
+     * @return false Allocation failed
+     */
+    virtual bool allocate_data_array();
+
+public:
+    
+    /**
+     * @brief Construct a new fft backend template object
+     * @note !! constructor has to call allocate_data_array
+     * 
+     * @param sample_size 
+     * @param frequency 
+     * @param bits 
+     */
+    FFT_backend_template( uint8_t input_pin, uint16_t sample_size, uint16_t frequency, fft_backend bits )
+    : m_sample_size(sample_size)
+    {
+    }
+
+    /**
+     * @brief Calculates fft & returns the loudest hz.
+     * 
+     * @return uint16_t 
+     */
+    virtual uint16_t calculate() = 0;
+    
+    /**
+     * @brief Gets the fft implementation specific sampling interrupt vector address.
+     * 
+     * @return void*
+     */
+    virtual vector_t get_read_vector() = 0;
+    
+    virtual ~FFT_backend_template() = default;
+};
+
+#endif
