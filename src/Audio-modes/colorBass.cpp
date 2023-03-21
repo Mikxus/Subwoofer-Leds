5ffcfbce9743d61a44a65456621d1288033d12cb
Mikxus <91991837+Mikxus@users.noreply.github.com>
2023-03-21 08:33:46 +0200

    Fixed bug
    Formatted
    
    bug fixed: blue led values not saved -> blue colors would stay on

--- src/Audio-modes/colorBass.cpp
+++ src/Audio-modes/colorBass.cpp
@@ -1,18 +1,18 @@
 /*
  * MIT License
- * 
- * Copyright (c) 2022 Mikko Johannes Heinänen 
- * 
+ *
+ * Copyright (c) 2022 Mikko Johannes Heinänen
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -20,11 +20,11 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
-*/
+ */
 #include "colorBass.h"
 
 colorBass::colorBass()
-: fft_obj(0, 64, 700, fixed_8)
+    : fft_obj(0, 64, 700, fixed_8)
 {
 }
 
@@ -33,73 +33,82 @@ colorBass::colorBass()
  *        of the input signal.
  *
  *
- * @returns bool: 1 If value has changed. 
+ * @returns bool: 1 If value has changed.
  */
 bool colorBass::Update()
 {
     uint16_t freq = 0;
+    uint16_t brightness = analogRead(_ledStrip->inputPin);
 
-    if (_update == 1)
+    if (_update)
     {
         freq = fft_obj.calculate();
     }
 
-    if (freq == 0) freq = _lastFreq;
-    else { _lastFreq = freq; DEBUG(F("Freq: "), freq); }    
-
-    uint16_t brightness = analogRead(_ledStrip->inputPin);
-
-    if (brightness <= _ledStrip->inputCalZero) freq = _lastFreq;
+    if (freq == 0)
+        freq = _lastFreq;
+    else
+    {
+        _lastFreq = freq;
+        DEBUG(F("Freq: "), freq);
+    }
 
     brightness = constrain(brightness, _ledStrip->inputCalZero, 800);
-    brightness = map(brightness, _ledStrip->inputCalZero,550,0,255);
-    
+    brightness = map(brightness, _ledStrip->inputCalZero, 550, 0, 255);
+
     /* if no change in brightness */
     if (brightness == _lastBrightness)
     {
         /* check if fade is complete */
-        if (! fade(freq, (uint8_t) brightness) ) 
+        if (!fade(freq, (uint8_t)brightness))
         {
             _update = 0;
             return _update;
-        } 
+        }
         _update = 1;
         return _update;
     }
 
-    if (_update == 0) _update = 1;
-
+    /* Brightness changed */
     _lastBrightness = brightness;
 
-    /* Applies smoothing to color & brightness. 
-    Then applies resulting color to ledArr */
-    return fade(freq, brightness);
+    _update = fade(freq, brightness);
+    return _update;
 }
-
 
 /**
  * @brief Smoothens the inputted values
- * 
- * @param hue 
- * @param brightness 
+ *
+ * @param hue
+ * @param brightness
  */
-uint8_t colorBass::fade(uint16_t hue, uint16_t brightness) {
+uint8_t colorBass::fade(uint16_t hue, uint16_t brightness)
+{
 
     /* Smoothen the input values */
-    float val = bright2.calc( bright1.calc((float)brightness) );                   // Calculate the smoothing
+    float val = bright1.calc((float)brightness);
     float colorVal = color_smooth.calc((float)hue);
-    
+
     /* Limit color value */
-    if ( colorVal > 255 ) colorVal = 255;
-    if ( colorVal < 0 ) colorVal = 0;
+    if (colorVal > 255)
+        colorVal = 255;
+    if (colorVal < 0)
+        colorVal = 0;
 
     /* Fill ledArr with the smoothed values */
-    fill_solid( _ledStrip->ledArr, _ledStrip->arrSize, ColorFromPalette( _ledStrip->colorPalette, (uint8_t) colorVal,(uint8_t) val, LINEARBLEND ));   // Sets whole strip to same color using a color palette
+    fill_solid(_ledStrip->ledArr, _ledStrip->arrSize,
+               ColorFromPalette(_ledStrip->colorPalette,
+                                (uint8_t)colorVal,
+                                (uint8_t)val,
+                                LINEARBLEND));
 
-    /* Check if any rgb values have changed */
-    if ( _ledStrip->ledArr->r == m_last_r && _ledStrip->ledArr->g == m_last_g && _ledStrip->ledArr->b == m_last_b ) return 0;   // if no value changed return 0
+    /* Check if no rgb values have changed */
+    if (_ledStrip->ledArr->r == m_last_r && _ledStrip->ledArr->g == m_last_g && _ledStrip->ledArr->b == m_last_b)
+        return 0;
 
-    logLastValue( _ledStrip->ledArr->r , _ledStrip->ledArr->g, _ledStrip->ledArr->r );                       // save the last hsv values
+    logLastValue(_ledStrip->ledArr->r,
+                 _ledStrip->ledArr->g,
+                 _ledStrip->ledArr->b);
 
     return 1;
 }
