de46226618e7e39134d4f09b7a693d26f1c091e5
Mikxus <91991837+Mikxus@users.noreply.github.com>
2023-10-03 20:15:17 +0300

    deprecated stuff
    started work on changing the ledStrip structs role. It should contain stuff a led strip would need. And then move all audioMode specific stuff to the audiomode class

--- src/utils/ledStrip.h
+++ src/utils/ledStrip.h
@@ -30,42 +30,61 @@
 #include "../colorPalettes.h"
 #include "../Audio-modes/audioModes.h"
 #include "virtual_led_array.h"
+#include "singly_linked_list.h"
 #include "debug.h"
 
-struct ledStrip                     // Struct for new led strips
+/**
+ * @brief singly linked list for ledStrip
+ * 
+ */
+struct singly_link_ledStrip : public singly_link_base
 {
-    ledStrip() = default;
+    ledStrip* next_ledStrip = nullptr;
+}
 
-    CRGB * led_rgb_data = nullptr;                // Pointer for the led array
-    uint16_t led_rgb_data_size = 0;                   // Number of leds on a single strip
-    ledStrip* next = nullptr;               // next ledStrip object
-    audioMode *effect_ptr = nullptr;        // Pointer to audio mode class
-    CRGBPalette16 colorPalette = CRGBPalette16(CRGB::Black);
-    
-    uint8_t inputPin = 0;                   // input pin used for the led srip
-    uint16_t inputCalZero = 0;              // Min value above the noise level
-    uint16_t inputCalMax = 0;               // Max input value
+/**
+ * @brief Struct which contains info about a ledstrip
+ * 
+ */
+struct ledStrip : public singly_link_ledStrip
+{
+    audioMode *list_head = nullptr;
+    CRGB *led_rgb_data = nullptr;    // Pointer for the led array
+    uint16_t led_rgb_data_size = 0;  // Number of leds on a single strip
 
-    __attribute__ ((deprecated)) uint8_t identifier = 0; // value to associate strip later
-    __attribute__ ((deprecated)) uint8_t mode = 0 ;         // contains light mode index
-    __attribute__ ((deprecated)) uint8_t brightness = 255; // brightness scale     255 is equal to 100% of the led's brightness
-    __attribute__ ((deprecated)) uint8_t paletteIndex = 0; // current palette id
+
+    uint8_t inputPin = 0;      // input pin used for the led srip
+    uint16_t inputCalZero = 0; // Min value above the noise level
+    uint16_t inputCalMax = 0;  // Max input value
+
 
     /* Methods for controlling the strip */
-    void SetBrightness(uint8_t value);
-    bool SetColor(uint8_t index);
-    __attribute__ ((deprecated)) bool SetMode(uint8_t modeIndex, CFastLED *fptr);
+    void set_brightness(uint8_t value);
+    bool set_colorpalette(uint8_t index);
 
-    bool loadObj(audioMode *ptr, CFastLED *fptr);
+    bool add_effect(audioMode *ptr, CRGB *led_array_start, CRGB *led_array_end);
+    bool remove_effect(audioMode* ptr);
 
+    ledStrip() = default;
     ~ledStrip();
 
-protected:
-    __attribute__ ((deprecated)) bool dynAllocated = 0;                  // true if CRGB *led_rgb_data is allocated by the ledStrip struct
+    /* Deprecated stuff */
+    __attribute__((deprecated)) uint8_t identifier = 0;   // value to associate strip later
+    __attribute__((deprecated)) uint8_t mode = 0;         // contains light mode index
+    __attribute__((deprecated)) uint8_t brightness = 255; // brightness scale     255 is equal to 100% of the led's brightness
+    __attribute__((deprecated)) uint8_t palette_index = 0; // current palette id
+    __attribute__((deprecated)) audioMode *effect_ptr_head = nullptr; // Pointer to audio mode class
+    __attribute__((deprecated)) CRGBPalette16 colorPalette = CRGBPalette16(CRGB::Black);
 
-    bool SetColorPalette(uint8_t colorIndex);
-    bool loadMode(uint8_t modeIndex, CFastLED *fptr); // "assings" mode to given audioMode object
-    void unloadMode();
+    __attribute__((deprecated)) bool SetMode(uint8_t modeIndex, CFastLED *fptr);
+
+protected:
+    /* Pointer to next ledStrip object */
+    ledStrip *next = nullptr;
+
+    __attribute__((deprecated)) bool dynamically_allcated = 0; // true if CRGB *led_rgb_data is allocated by the ledStrip struct
+    __attribute__((deprecated)) bool set_color_palette(uint8_t colorIndex);
+    __attribute__((deprecated)) bool load_mode(uint8_t modeIndex, CFastLED *fptr); // "assings" mode to given audioMode object
 };
 #include "../Audio-modes/colorBass.h"
 #endif
