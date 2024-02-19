bb8485848faf437f6fba6d690d95de5506db4ffb
Mikxus <91991837+Mikxus@users.noreply.github.com>
2024-02-19 20:32:20 +0200

    chore: add better comments
    chore: add two instances of the simple_rainbow effect to demonstrate the virtual array ability

--- examples/simple_rainbow/simple_rainbow.ino
+++ examples/simple_rainbow/simple_rainbow.ino
@@ -5,30 +5,40 @@
 /* Definitions for the led strip */
 #define NUM_LEDS 58         // number of leds
 #define DATA_PIN 6          // data pin for leds
-#define AUDIO_PIN 0         // analog pin for the led strip
 #define LED_CHIPSET WS2812B // Led chipset
 #define COLOR_ORDER GRB     // led strip's color order: rgb grb brg
-/* ----------------------------- */
 
-led_manager effect_mgr; // Create SubEffects object then pass CFastLED to it
+/**
+ * Led manager object 
+ *  Represents a all the led strips and their effects
+ */
+led_manager effect_mgr;
 
-ledStrip led_strip; // led strip object
+/**
+ *  Led strip struct
+ *  the ledStrip struct represents a single physical led strip
+ */
+ledStrip led_strip; 
 
-CRGB leds[NUM_LEDS]; // led array for the led strip
+/* Led array for the led strip */
+CRGB leds[NUM_LEDS];
 
 /**
  * @brief example of a simple audio mode.
- *
  */
 class simple_rainbow : public audioMode
 {
+public:
   uint8_t hue = 0;
   uint8_t speed = 80;
 
-public:
   simple_rainbow() = default;
   ~simple_rainbow() = default;
 
+    /* update function which draws a single "frame of the pixels"  
+     * Note: It uses led_array to access the CRGB pixels 
+     * to enable changing of the effect's pixel output range easily
+     */
     virtual bool update()
     {
         fill_rainbow(&led_array[0], led_array.size(), beat8(speed, 255), 10 );
@@ -37,10 +47,10 @@ public:
 };
 
 simple_rainbow rainbow_obj;
+simple_rainbow rainbow_obj_fast;
 
 void setup()
 {
-  pinMode(AUDIO_PIN, INPUT);
   pinMode(DATA_PIN, OUTPUT);
 
   Serial.begin(38400);
@@ -55,8 +65,15 @@ void setup()
 
   effect_mgr.add_led_strip(led_strip, &leds[0], NUM_LEDS);
 
-  /* Now load our own led effect */
-  effect_mgr.add_effect(led_strip, rainbow_obj);
+  /* 
+   * Now load our own led effects 
+   * And set the effects output range the whole strip
+   */
+  effect_mgr.add_effect(led_strip, rainbow_obj, &leds[0], &leds[29]);
+
+  /* set rainbow_obj_fast's animation speed really fast */
+  rainbow_obj_fast.speed = 200;
+  effect_mgr.add_effect(led_strip, rainbow_obj_fast, &leds[29], &leds[58]);
 }
 
 /* Updates the ledstrip */
